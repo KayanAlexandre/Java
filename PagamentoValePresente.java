066557bd270ca5e7a906c3416be955b4c7ce21e6
Kayan Alexandre de Oliveira Rodrigues <alexandrekayan7@gmail.com>
2026-09-02 20:40:00 -0300

    Delete PagamentoValePresente.java

--- PagamentoValePresente.java
+++
@@ -1,7 +0,0 @@
-public class PagamentoValePresente implements FormaPagamento{
-    @Override
-    public void pagar(double valor){
-        System.out.printf("Utilizando R$ %2.f do vale-presente.%n",valor);
-    }
-    
-}
