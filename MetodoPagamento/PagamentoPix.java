75796a635bcdb5fefc6ebfd5485b6782bf762387
Kayan Alexandre de Oliveira Rodrigues <alexandrekayan7@gmail.com>
2026-09-19 15:17:40 -0300

    Implement PagamentoPix class for Pix payment processing

--- MetodoPagamento/PagamentoPix.java
+++ MetodoPagamento/PagamentoPix.java
@@ -1,3 +1,16 @@
+import java.math.Bigdecimal;
+import java.util.UUID;
+
 public class PagamentoPix {
-    
+    public PagamentoPix(BigDecimal valor){
+        super(valor);
+    }
+   @Override
+    public boolean processar(){
+        String chavePix=UUID.random().toString();
+        this.status="AGUARDANDO_PAGAMENTO";
+        System.out.println("Pix gerado no valor de "+this.valor+".");
+        System.out.println("Copie e cole a chave: " + chavePix);
+        return true ; // Simula que a geração deu certo
+    } 
 }
