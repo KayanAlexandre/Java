89735063e34fb63573ab828b76a7d55670a8305f
Kayan Alexandre de Oliveira Rodrigues <alexandrekayan7@gmail.com>
2026-09-19 15:23:28 -0300

    Implement checkout process with payment method handling

--- MetodoPagamento/Checkout.java
+++ MetodoPagamento/Checkout.java
@@ -1,3 +1,15 @@
 public class Checkout {
-    
+    //POLIMORFISMO
+    //Este Método não sabe se é cartão,Pix ou boleto.Ele apenas manda "processar".
+    public void finalizadorCompra(MetodoPagamento metodoPagamento) {
+        System.out.println("---Iniciando checkout---");
+        boolean sucesso = metodoPagamento.processar();
+
+        if(sucesso){
+            System.out.println("Compra finalizada. Status do pedido: "+ metodoPagamento.getStatus());
+        } else{
+            System.out.println("Falha no checkout. Verifique o método de pagamento.");
+        }
+        System.out.println("-------------------\n);
+    }
 }
